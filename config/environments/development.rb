10dda894f785d6ca936340d171990d3c22880813
JientDoNgoc <dongocthuyoanh.07.05@gmail.com>
2018-04-15 09:23:28 +0000

    registration email config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,4 +53,17 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true # to raise error if smtp has error on setup
+  config.action_mailer.default :charset => "utf-8"
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => "587",
+    :domain => "gmail.com",
+    :user_name => "jientdongocsp@gmail.com",
+    :password => "Jient1996",
+    :authentication => "plain",
+    :enable_starttls_auto => true
+  } 
 end
